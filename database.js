0af91cab81dffa63bfd1cc04e7d071cc6a021073
Angelo Sabornido <angelosabornido992@gmail.com>
2025-08-26 16:45:26 +0800

    Update For Deplpoyment

--- database.js
+++ database.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2/promise');
 
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: 'root',
-  password: 'amine_*2019sabornido',
-  database: 'accounts',
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
